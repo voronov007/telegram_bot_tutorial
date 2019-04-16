96a570edd0a55c74badc6b50d65108f149326416
Andrew Voronov <a.voronov@lukkien.com>
2019-04-16 16:51:51 +0200

    * final updates;

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tb_tutorial.wsgi
--- tb_tutorial/settings/production.py
+++ tb_tutorial/settings/production.py
@@ -6,9 +6,11 @@ from tb_tutorial.settings.base import *  # noqa
 
 DEBUG = False
 
+ALLOWED_HOSTS.append("tb-tutorial.herokuapp.com")
+
 mongodb_user = os.getenv("MONGO_USER")
 mongodb_password = os.getenv("MONGO_PASSWORD")
 MONGO_CLIENT = MongoClient(
     f"mongodb://{mongodb_user}:{mongodb_password}@ds027744.mlab.com:27744/tb_tutorial"
 )
-MONGO_DB = MONGO_CLIENT.telegram_bots
+MONGO_DB = MONGO_CLIENT.tb_tutorial
